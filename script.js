36bd5730a658af71323fee34d0235fdc11717a7d
adrian-alt625 <adriandinkov625@gmail.com>
2025-02-09 13:27:05 +0000

    Working "upload" button

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+//do crtl+shift+g to open the "commit menu" to submit the updated version to GitHub
 "use strict";
 
-console.log("hello world");
+//assigning values for the "upload button" and the file input
+const fileInput = document.getElementById("fileInput");
+const upload = document.getElementById("uploadBtn");
+
+//initiating file input (through file explorer) once the "upload" button is pressed
+upload.addEventListener("click", openFE);
+
+//what happens when "upload" button is pressed
+function openFE() {
+  console.log("hello world");
+  fileInput.click();
+}
+
+//calls the "readFile" function when a file is uploaded
+fileInput.addEventListener("change", readFile);
+
+//reading the file, creating an array where each line is stored seperately in an array
+function readFile() {
+  console.log("hi guys");
+
+  let reader = new FileReader();
+
+  reader.readAsText(fileInput.files[0]);
+
+  reader.onload = function () {
+    let text = reader.result;
+
+    text = text.split("\n");
+    text = text.map((line) => line.trim());
+    console.log(text);
+  };
+}
+
+/* PUSH TO GITHUB */
